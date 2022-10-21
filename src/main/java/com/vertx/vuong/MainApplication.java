e2d8f4c5f1f174b9988e46033545670bcfe03051
vuongbv0105 <vuongbv0105@gmail.com>
2022-10-21 10:24:32 +0700

    uod

--- src/main/java/com/vertx/vuong/MainApplication.java
+++ src/main/java/com/vertx/vuong/MainApplication.java
@@ -27,6 +27,7 @@ import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.SLF4JLogDelegateFactory;
 
 // Clustering dựa vào lệnh java -jar target/vertx-dev-1.0.0-SNAPSHOT.jar --conf config/testconfig1.json -cluster -Djava.net.preferIPv4Stack=true -Dhttp.port=8090
 // Đây là khai báo cluster nếu chạy dựa trên đối tượng vertx của hệ thống tạo ra sẵn, còn nếu tự tạo vertx riêng thì phải cấu hình cluster
@@ -44,7 +45,9 @@ public class MainApplication  {
 		
 		System.out.println(String.format("[%s] Start MainApplication--------------", Thread.currentThread().getName()));
 		
-		loadLogging();
+//		loadLogging();
+		
+//		System.setProperty("vertx.logger-delegate-factory-class-name", SLF4JLogDelegateFactory.class.getName());
 		
 		Vertx vertx = Vertx.vertx(new VertxOptions().setPreferNativeTransport(true).setEventLoopPoolSize(10).setWorkerPoolSize(3).setBlockedThreadCheckInterval(5 * 10 * 1000));
 		
