654429cd3a2af5486575d7e9493f53e2352f351f
vuongbv0105 <vuongbv0105@gmail.com>
2022-10-28 16:28:08 +0700

    cluster

--- src/main/java/com/vertx/vuong/MainApplication.java
+++ src/main/java/com/vertx/vuong/MainApplication.java
@@ -3,6 +3,7 @@ package com.vertx.vuong;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.hazelcast.config.Config;
 import com.vertx.vuong.verticle.DatabaseVerticle;
 import com.vertx.vuong.verticle.GatewayVerticle;
 import com.vertx.vuong.verticle.GrpcClientVerticle;
@@ -22,12 +23,13 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.ext.cluster.infinispan.InfinispanClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 
 // Clustering dựa vào lệnh java -jar target/vertx-dev-1.0.0-SNAPSHOT.jar --conf config/testconfig1.json -cluster -Djava.net.preferIPv4Stack=true -Dhttp.port=8090
 // Đây là khai báo cluster nếu chạy dựa trên đối tượng vertx của hệ thống tạo ra sẵn, còn nếu tự tạo vertx riêng thì phải cấu hình cluster
 // Cluster application phân tải công việc đến từng node thông qua eventbus
 // java -jar target/vertx-dev-1.0.0-SNAPSHOT.jar --conf config/testconfig1.json --options config/vertxoption.json
+// java -Dvertx.hazelcast.config=./config/cluster.xml -Dhttp.port=8082 -jar target/vertx-dev-1.0.0-SNAPSHOT-run.jar -cluster -Djava.net.preferIPv4Stack=true
 public class MainApplication  {
 	
 	private static final Logger LOGGER = LogManager.getLogger(MainApplication.class);
@@ -42,7 +44,13 @@ public class MainApplication  {
 		
 //		Vertx vertx = Vertx.vertx(new VertxOptions().setPreferNativeTransport(true).setEventLoopPoolSize(10).setWorkerPoolSize(3).setBlockedThreadCheckInterval(5 * 10 * 1000));
 		
-		ClusterManager mgr = new InfinispanClusterManager();
+//		ClusterManager mgr = new InfinispanClusterManager();
+		
+		Config hazelcastConfig = new Config();
+		
+		// some configuration settings
+		ClusterManager mgr = new HazelcastClusterManager(hazelcastConfig);
+
 		
 		VertxOptions options = new VertxOptions().setClusterManager(mgr);;
 		
@@ -104,26 +112,26 @@ public class MainApplication  {
 		});
 		
 		Future<String> futureHello = Future.future(promise -> {
-			vertx.deployVerticle(HelloVerticle.class.getName(), new DeploymentOptions().setWorker(false).setInstances(1).setConfig(jsonObject), promise);
+			vertx.deployVerticle(HelloVerticle.class.getName(), new DeploymentOptions().setWorker(false).setInstances(2).setConfig(jsonObject), promise);
 		});
 		
-		Future<String> futureTcpServer = Future.future(promise -> {
-			vertx.deployVerticle(TcpServerVerticle.class.getName(), new DeploymentOptions().setWorker(false).setConfig(jsonObject), promise);
-		});
+//		Future<String> futureTcpServer = Future.future(promise -> {
+//			vertx.deployVerticle(TcpServerVerticle.class.getName(), new DeploymentOptions().setWorker(false).setConfig(jsonObject), promise);
+//		});
 		
-		Future<String> futureDatabase = Future.future(promise -> {
-			vertx.deployVerticle(DatabaseVerticle.class.getName(), new DeploymentOptions().setWorker(false).setConfig(jsonObject), promise);
-		});
+//		Future<String> futureDatabase = Future.future(promise -> {
+//			vertx.deployVerticle(DatabaseVerticle.class.getName(), new DeploymentOptions().setWorker(false).setConfig(jsonObject), promise);
+//		});
 		
-		Future<String> futureGrpcServer = Future.future(promise -> {
-			vertx.deployVerticle(GrpcServerVerticle.class.getName(), new DeploymentOptions().setInstances(1).setConfig(jsonObject), promise);
-		});
+//		Future<String> futureGrpcServer = Future.future(promise -> {
+//			vertx.deployVerticle(GrpcServerVerticle.class.getName(), new DeploymentOptions().setInstances(1).setConfig(jsonObject), promise);
+//		});
 		
-		Future<String> futureGrpcClient = Future.future(promise -> {
-			vertx.deployVerticle(GrpcClientVerticle.class.getName(), new DeploymentOptions().setInstances(1).setConfig(jsonObject), promise);
-		});
+//		Future<String> futureGrpcClient = Future.future(promise -> {
+//			vertx.deployVerticle(GrpcClientVerticle.class.getName(), new DeploymentOptions().setInstances(1).setConfig(jsonObject), promise);
+//		});
 		
-		return CompositeFuture.all(futureGw, futureHello, futureTcpServer, futureDatabase, futureGrpcServer, futureGrpcClient).mapEmpty();
+		return CompositeFuture.all(futureGw, futureHello).mapEmpty();
 	}
 	
 }
